fd355a036b8b6341b23c66882304fd466c3d7c1d
Gianluca <gianlucadorelo@gmail.com>
2021-05-14 17:22:49 -0300

    Update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,12 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-  let checkedAmenities = {};
-=======
   const checkedAmenities = {};
->>>>>>> ee3c97aae3b9903935f8a93ff113599d6d70dc40
   $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
       checkedAmenities[$(this).data('id')] = $(this).data('name');
     } else {
       delete checkedAmenities[$(this).data('id')];
     }
-<<<<<<< HEAD
-    let lst = Object.values(checkedAmenities);
-=======
     const lst = Object.values(checkedAmenities);
->>>>>>> ee3c97aae3b9903935f8a93ff113599d6d70dc40
     if (lst.length === 0) {
       $('.amenities > h4').html('&nbsp;');
     } else {
