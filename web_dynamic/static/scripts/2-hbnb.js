deec0986e96d24fb7419e44f642ca49b1b8eafad
martin saavedra <martin@bildung.uy>
2021-05-15 20:31:17 +0000

    task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,26 +1,24 @@
-$('document').ready(function () {
-  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
-  $.get(url, function (response) {
-    if (response.status === 'OK') {
-      $('DIV#api_status').addClass('available');
+$(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, statusText, xhr) {
+    if (xhr.status === 200) {
+      $('#api_status').addClass('available');
+    }
+    else {
+      $('#api_status').removeClass('available');
+    }
+  })
+  const checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
     } else {
-      $('DIV#api_status').removeClass('available');
+      delete checkedAmenities[$(this).data('id')];
+    }
+    const lst = Object.values(checkedAmenities);
+    if (lst.length === 0) {
+      $('.amenities > h4').html('&nbsp;');
+    } else {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
     }
   });
-
-    let checkedAmenities = {};
-    $(document).on('change', "input[type='checkbox']", function () {
-      if (this.checked) {
-        checkedAmenities[$(this).data('id')] = $(this).data('name');
-      } else {
-        delete checkedAmenities[$(this).data('id')];
-      }
-      let lst = Object.values(checkedAmenities);
-      if (lst.length === 0) {
-        $('.amenities > h4').html('&nbsp;');
-      } else {
-        $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
-      }
-    });
-  });
-  
+});
