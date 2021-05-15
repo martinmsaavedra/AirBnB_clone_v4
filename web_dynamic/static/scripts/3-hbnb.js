539d1310d3417e48ef60ca9a8d6db67aa4e2dfa8
martin saavedra <martin@bildung.uy>
2021-05-15 20:35:31 +0000

    Task 3

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,12 +1,14 @@
-$(document).ready(function () {
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, statusText, xhr) {
-      if (xhr.status === 200) {
-        $('#api_status').addClass('available');
-      }
-      else {
-        $('#api_status').removeClass('available');
-      }
-    })
+$('document').ready(function () {
+  const api = 'http://' + window.location.hostname;
+
+  $.get(api + ':5001:/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
     const checkedAmenities = {};
     $(document).on('change', "input[type='checkbox']", function () {
       if (this.checked) {
