23d72dfa50e4d4838b7cbd3716baf5933a9a97b8
martin saavedra <martin@bildung.uy>
2021-05-15 21:41:51 +0000

    Task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,49 +1,59 @@
-$('document').ready(function () {
-  const api = 'http://' + window.location.hostname;
-
-  $.get(api + ':5001:/api/v1/status/', function (response) {
-    if (response.status === 'OK') {
-      $('DIV#api_status').addClass('available');
-    } else {
-      $('DIV#api_status').removeClass('available');
+$(document).ready(() => {
+  const amenCheckList = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).is(':checked')) {
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      amenCheckList[key] = value;
     }
+    if (!$(this).is(':checked')) {
+      delete amenCheckList[$(this).attr('data-id')];
+    }
+    const list = [];
+    for (const name in amenCheckList) {
+      list.push(amenCheckList[name]);
+    }
+    $('div.amenities h4').text(list.join(', '));
   });
+});
 
-  $.ajax({
-    url: 'http://127.0.0.1:5001/api/v1/places_search/',
-    method: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({}),
-    success: data => {
-      data.map(place => {
-        $('.places h1').after(`
-              <article>
-              <div class="title_box">
-              <h2>${place.name}</h2>
-              <div class="price_by_night">${place.price_by_night}</div>
-              </div>
-              <div class="information">
-                  <div class="max_guest">${place.max_guest} Guest${plurals(place.max_guest)}</div>
-                  <div class="number_rooms">${place.number_rooms} Bedroom${plurals(place.number_rooms)}</div>
-                  <div class="number_bathrooms">${place.number_bathrooms} Bathroom${plurals(place.number_bathrooms)}</div>
-              </div>
-          <div class="description">${place.description} </div>
-              </article>`);
-      });
-    }});
-    
-  const checkedAmenities = {};
-  $(document).on('change', "input[type='checkbox']", function () {
-    if (this.checked) {
-      checkedAmenities[$(this).data('id')] = $(this).data('name');
-    } else {
-      delete checkedAmenities[$(this).data('id')];
-    }
-    const lst = Object.values(checkedAmenities);
-    if (lst.length === 0) {
-      $('.amenities > h4').html('&nbsp;');
-    } else {
-      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
-    }
-  });
+function plurals (n) {
+  if (n > 1) {
+    return 's';
+  }
+  return '';
+}
+
+/* Check Api status */
+$.getJSON('http://127.0.0.1:5001/api/v1/status', data => {
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
+
+/* Search Place */
+$.ajax({
+  url: 'http://127.0.0.1:5001/api/v1/places_search/',
+  method: 'POST',
+  contentType: 'application/json',
+  data: JSON.stringify({}),
+  success: data => {
+    data.map(place => {
+      $('.places h1').after(`
+            <article>
+            <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+                <div class="max_guest">${place.max_guest} Guest${plurals(place.max_guest)}</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${plurals(place.number_rooms)}</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${plurals(place.number_bathrooms)}</div>
+            </div>
+        <div class="description">${place.description} </div>
+            </article>`);
+    });
+  }
 });
---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,80 @@
+$(document).ready(() => {
+    const amenCheckList = {};
+    $('input[type=checkbox]').change(function () {
+      if ($(this).is(':checked')) {
+        const key = $(this).attr('data-id');
+        const value = $(this).attr('data-name');
+        amenCheckList[key] = value;
+      }
+      if (!$(this).is(':checked')) {
+        delete amenCheckList[$(this).attr('data-id')];
+      }
+      const list = [];
+      for (const name in amenCheckList) {
+        list.push(amenCheckList[name]);
+      }
+      $('div.amenities h4').text(list.join(', '));
+    });
+  
+    /* Filter Place */
+    $('button').click(() => {
+      const amenR = { amenities: [] };
+      $('article').remove();
+      for (const key of Object.keys(amenCheckList)) {
+        amenR.amenities.push(key);
+      }
+      $.ajax({
+        url: 'http://127.0.0.1:5001/api/v1/places_search',
+        method: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(amenR),
+        success: data => articles(data)
+      });
+    });
+  
+    /* Check Api status */
+    $.getJSON('http://127.0.0.1:5001/api/v1/status', data => {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+  
+    /* Search Place */
+    $.ajax({
+      url: 'http://127.0.0.1:5001/api/v1/places_search/',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: data => articles(data)
+    });
+  
+    const articles = data => {
+      $('.places h1').html('Total places: ' + data.length);
+      data.map(place => {
+        $('.places h1').after(`
+        <article>
+          <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${plurals(place.max_guest)}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${plurals(place.number_rooms)}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${plurals(place.number_bathrooms)}</div>
+          </div>
+          <div class="description">${place.description}</div>
+        </article>`);
+      });
+    };
+  });
+  
+  function plurals (n) {
+    if (n > 1) {
+      return 's';
+    }
+    return '';
+  }
+  
